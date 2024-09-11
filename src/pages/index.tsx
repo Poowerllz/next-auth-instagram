b95ee0da0a7f31da01e8761d0af778ad1f7c1ca5
Poowerllz <safilhoelzevirsafilho@gmail.com>
2024-09-10 21:54:13 -0300

    feat: setup next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { signOut } from "next-auth/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -53,14 +54,13 @@ export default function Home() {
             />
             Deploy now
           </a>
-          <a
+          <button
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            onClick={() => signOut()}
           >
-            Read our docs
-          </a>
+            Logout
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
